6eeefbcd7c05acddf4dafb4eaf19e3b981dea181
jonnystv <jonathan@darefoundation.com>
2020-11-21 16:17:03 +0000

    Test_ 2 Passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,9 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# TEST 1
+# TEST_1
 def get_pet_shop_name(shop):
     return shop["name"]
 
-#TEST 2
+#TEST_2
 def get_total_cash(cash_total):
     return cash_total["admin"]["total_cash"]
