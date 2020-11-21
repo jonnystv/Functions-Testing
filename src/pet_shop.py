588cb237abfffddf408b9389cce3ae00a0f65026
jonnystv <jonathan@darefoundation.com>
2020-11-21 18:16:38 +0000

    Test_3 Passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -7,3 +7,8 @@ def get_pet_shop_name(shop):
 #TEST_2
 def get_total_cash(cash_total):
     return cash_total["admin"]["total_cash"]
+
+
+#TEST_3
+def add_or_remove_cash(cash, amount):
+    cash["admin"]["total_cash"] += amount
