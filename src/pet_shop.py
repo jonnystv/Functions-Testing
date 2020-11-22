ff696ad6286adc8d7f1f8aee551a9207219301a8
jonnystv <jonathan@darefoundation.com>
2020-11-22 12:54:37 +0000

    Test 5

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,11 +4,17 @@
 def get_pet_shop_name(shop):
     return shop["name"]
 
+
 #TEST_2
 def get_total_cash(cash_total):
     return cash_total["admin"]["total_cash"]
 
 
-#TEST_3
+#TEST_3 & 4
 def add_or_remove_cash(cash, amount):
     cash["admin"]["total_cash"] += amount
+
+
+#TEST_5
+def get_pets_sold(pets_sold):
+    return pets_sold["admin"]["pets_sold"]
